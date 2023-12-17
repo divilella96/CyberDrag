ffe65303661fb449f61ba60ad96935398e63481a
Diego Vilella Rodrigues <divilella@gmail.com>
2023-12-17 13:28:06 -0300

    Initial commit

---
+++ projetoTesteConsole/TabelaVerdade/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class TabelasVerdade {
+    static void Main() {
+        MostrarTabelaAnd();
+        MostrarTabelaOr();
+        MostrarTabelaNot();
+        MostrarTabelaXor();
+    }
+
+    static void MostrarTabelaAnd() {
+        Console.WriteLine("Tabela Verdade para P E Q E R (AND):");
+        Console.WriteLine("P | Q | R | P E Q E R");
+        Console.WriteLine("--|---|---|------------");
+        for (int p = 0; p <= 1; p++) {
+            for (int q = 0; q <= 1; q++) {
+                for (int r = 0; r <= 1; r++) {
+                    bool resultado = p == 1 && q == 1 && r == 1;
+                    Console.WriteLine($"{p} | {q} | {r} | {resultado}");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static void MostrarTabelaOr() {
+        Console.WriteLine("Tabela Verdade para P OU Q OU R (OR):");
+        Console.WriteLine("P | Q | R | P OU Q OU R");
+        Console.WriteLine("--|---|---|-------------");
+        for (int p = 0; p <= 1; p++) {
+            for (int q = 0; q <= 1; q++) {
+                for (int r = 0; r <= 1; r++) {
+                    bool resultado = p == 1 || q == 1 || r == 1;
+                    Console.WriteLine($"{p} | {q} | {r} | {resultado}");
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static void MostrarTabelaNot() {
+        Console.WriteLine("Tabela Verdade para NÃO P:");
+        Console.WriteLine("P | ~P");
+        Console.WriteLine("--|---");
+        for (int p = 0; p <= 1; p++) {
+            bool resultado = p == 0;
+            Console.WriteLine($"{p} | {resultado}");
+        }
+        Console.WriteLine();
+    }
+
+    static void MostrarTabelaXor() {
+        Console.WriteLine("Tabela Verdade para P XOR Q:");
+        Console.WriteLine("P | Q | P XOR Q");
+        Console.WriteLine("--|---|---------");
+        for (int p = 0; p <= 1; p++) {
+            for (int q = 0; q <= 1; q++) {
+                bool resultado = p != q;
+                Console.WriteLine($"{p} | {q} | {resultado}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
---
+++ projetoTesteConsole/VerificarParImpar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class TabelasVerdade {
+    static void Main() {
+        MostrarTabelaAnd();
+        MostrarTabelaOr();
+    }
+
+    static void MostrarTabelaAnd() {
+        Console.WriteLine("Tabela Verdade para P E Q (AND):");
+        Console.WriteLine("P | Q | P E Q");
+        Console.WriteLine("--|---|-----");
+        for (int p = 0; p <= 1; p++) {
+            for (int q = 0; q <= 1; q++) {
+                bool resultado = p == 1 && q == 1;
+                Console.WriteLine($"{p} | {q} | {resultado}");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static void MostrarTabelaOr() {
+        Console.WriteLine("Tabela Verdade para P OU Q (OR):");
+        Console.WriteLine("P | Q | P OU Q");
+        Console.WriteLine("--|---|------");
+        for (int p = 0; p <= 1; p++) {
+            for (int q = 0; q <= 1; q++) {
+                bool resultado = p == 1 || q == 1;
+                Console.WriteLine($"{p} | {q} | {resultado}");
+            }
+        }
+        Console.WriteLine();
+    }
+}
