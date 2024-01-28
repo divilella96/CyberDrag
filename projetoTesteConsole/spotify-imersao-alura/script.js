6a7f9df2ba5725901b5c13a6a797d2d8162c4d75
Diego Vilella Rodrigues <divilella@gmail.com>
2024-01-28 12:28:34 +0000

    corrigindo API

--- projetoTesteConsole/spotify-imersao-alura/script.js
+++ projetoTesteConsole/spotify-imersao-alura/script.js
@@ -3,7 +3,7 @@ const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm){
-    const url = `http://localhost:3030/artists?name_like=${searchTerm}`
+    const url = `https://api-artists-49zuvo4r8-divilella96.vercel.app/artists?name_like=${searchTerm}`
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
